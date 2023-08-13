a33a492b0fa8744f9578af621bdb0785a830e200
ShaliniGMCT <127105084+ShaliniGMCT@users.noreply.github.com>
2023-08-13 07:27:33 -0700

    Plugindemo code

---
+++ PreOperationFormatPhoneCreateUpdate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+
+using System.Text.RegularExpressions;
+
+namespace PloginDemo
+{
+    public class PreOperationFormatPhoneCreateUpdate : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            if (!context.InputParameters.ContainsKey("Target"))
+
+                throw new InvalidPluginExecutionException("No target found");
+            var entity = context.InputParameters["Target"] as Entity;
+
+            if (!entity.Attributes.Contains("telephone1"))
+
+                return;
+            string phoneNumber = (string)entity["telephone1"];
+
+            var formattedNumber = Regex.Replace(phoneNumber, @"[^\d]", "");
+            entity["telephone1"] = formattedNumber;
+        }
+    }
+}
